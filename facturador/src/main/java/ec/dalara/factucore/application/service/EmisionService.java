972653cf14ea2222ed9befbc877b7d8176ed6f2d
Danny Lamilla <ldanny_19@hotmail.com>
2026-09-22 14:13:37 -0500

    Clases parciales de WFK5

---
+++ facturador/src/main/java/ec/dalara/factucore/application/service/EmisionService.java
@@ -0,0 +1,47 @@
+package ec.dalara.factucore.application.service;
+
+import ec.dalara.factucore.infrastructure.persistence.entity.Establecimiento;
+import ec.dalara.factucore.infrastructure.persistence.entity.PuntoEmision;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmisionService {
+
+    private final EstablecimientoService establecimientoService;
+    private final PuntoEmisionService puntoEmisionService;
+
+    public ResultadoEmision resolver(
+            Long empresaId,
+            String codigoEstablecimiento,
+            String codigoPuntoEmision
+    ) {
+        Establecimiento establecimiento =
+                establecimientoService
+                        .obtenerPorEmpresaYCodigo(
+                                empresaId,
+                                codigoEstablecimiento
+                        )
+                        .orElseThrow();
+
+        PuntoEmision puntoEmision =
+                puntoEmisionService
+                        .obtenerPorEstablecimientoYCodigo(
+                                establecimiento.getId(),
+                                codigoPuntoEmision
+                        )
+                        .orElseThrow();
+
+        return new ResultadoEmision(
+                establecimiento,
+                puntoEmision
+        );
+    }
+
+    public record ResultadoEmision(
+            Establecimiento establecimiento,
+            PuntoEmision puntoEmision
+    ) {
+    }
+}
