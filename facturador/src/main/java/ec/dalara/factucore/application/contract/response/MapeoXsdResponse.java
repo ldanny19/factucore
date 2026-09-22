cc1f2608297ed61d24109abda5cdcb56540e7ba1
ldanny19 <103215792+ldanny19@users.noreply.github.com>
2026-09-21 19:47:30 -0500

    feat: agregar administracion de mapeos XSD

---
+++ facturador/src/main/java/ec/dalara/factucore/application/contract/response/MapeoXsdResponse.java
@@ -0,0 +1,27 @@
+package ec.dalara.factucore.application.contract.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MapeoXsdResponse {
+
+    private Long id;
+
+    private Long idVersionDocumentoXsd;
+
+    private String rutaOrigen;
+
+    private Long idElementoXsd;
+
+    private Long idAtributoXsd;
+
+    private String tipoMapeo;
+
+    private String estadoRegistro;
+}
