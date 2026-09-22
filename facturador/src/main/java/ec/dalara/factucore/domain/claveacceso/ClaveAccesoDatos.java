3aff83c5e2425656a157f6bc47f636a98b886e6a
ldanny19 <103215792+ldanny19@users.noreply.github.com>
2026-09-21 20:44:36 -0500

    refactor: ubicar datos de clave de acceso en dominio

---
+++ facturador/src/main/java/ec/dalara/factucore/domain/claveacceso/ClaveAccesoDatos.java
@@ -0,0 +1,16 @@
+package ec.dalara.factucore.domain.claveacceso;
+
+import java.time.LocalDate;
+
+public record ClaveAccesoDatos(
+        LocalDate fechaEmision,
+        String codigoDocumento,
+        String ruc,
+        String ambiente,
+        String establecimiento,
+        String puntoEmision,
+        String secuencial,
+        String codigoNumerico,
+        String tipoEmision
+) {
+}
