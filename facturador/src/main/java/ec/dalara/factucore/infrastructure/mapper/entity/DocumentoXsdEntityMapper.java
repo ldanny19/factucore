21da53248262c07af5e8fc24dbc3ff0d411213a8
ldanny19 <103215792+ldanny19@users.noreply.github.com>
2026-09-23 18:30:02 -0500

    feat(documento-xsd): propagar prefijo de archivo

--- facturador/src/main/java/ec/dalara/factucore/infrastructure/mapper/entity/DocumentoXsdEntityMapper.java
+++ facturador/src/main/java/ec/dalara/factucore/infrastructure/mapper/entity/DocumentoXsdEntityMapper.java
@@ -5,7 +5,7 @@ import ec.dalara.factucore.domain.documentoxsd.DocumentoXsdModel;
 import ec.dalara.factucore.infrastructure.persistence.entity.DocumentoXsd;
 @Mapper(config=ec.dalara.factucore.infrastructure.mapper.MapStructInfrastructureConfig.class)
 public interface DocumentoXsdEntityMapper {
- default DocumentoXsdModel toModel(DocumentoXsd e){if(e==null)return null;return new DocumentoXsdModel(e.getCodigo(),e.getNombre(),e.getDescripcion(),e.getTipoDocumento());}
+ default DocumentoXsdModel toModel(DocumentoXsd e){if(e==null)return null;return new DocumentoXsdModel(e.getCodigo(), e.getNombre(), e.getDescripcion(), e.getTipoDocumento(), e.getPrefijoArchivo());}
  @Mapping(target="id",ignore=true)
  
  DocumentoXsd toEntity(DocumentoXsdModel model);
