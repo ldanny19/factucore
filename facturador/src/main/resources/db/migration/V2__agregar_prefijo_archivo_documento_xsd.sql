ad92995051ae4c708c3b79dded6956a5e5c70d1a
ldanny19 <103215792+ldanny19@users.noreply.github.com>
2026-09-23 18:30:15 -0500

    fix(documento-xsd): inicializar prefijos segun codigo SRI

--- facturador/src/main/resources/db/migration/V2__agregar_prefijo_archivo_documento_xsd.sql
+++ facturador/src/main/resources/db/migration/V2__agregar_prefijo_archivo_documento_xsd.sql
@@ -2,8 +2,15 @@ ALTER TABLE documento_xsd
     ADD COLUMN prefijo_archivo VARCHAR(20);
 
 UPDATE documento_xsd
-SET prefijo_archivo = codigo
-WHERE prefijo_archivo IS NULL;
+SET prefijo_archivo = CASE codigo
+    WHEN '01' THEN 'FAC'
+    WHEN '04' THEN 'NC'
+    WHEN '05' THEN 'ND'
+    WHEN '06' THEN 'GR'
+    WHEN '07' THEN 'RET'
+    WHEN '08' THEN 'LC'
+    ELSE NULL
+END;
 
 ALTER TABLE documento_xsd
     ALTER COLUMN prefijo_archivo SET NOT NULL;
