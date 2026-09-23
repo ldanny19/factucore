e1bfdf41b1c560ec0d75c739ab9ac279da7d086e
ldanny19 <103215792+ldanny19@users.noreply.github.com>
2026-09-23 18:38:27 -0500

    feat(comprobante): actualizar contrato de evidencias

--- facturador/src/main/java/ec/dalara/factucore/application/port/out/ComprobanteEvidenciaPort.java
+++ facturador/src/main/java/ec/dalara/factucore/application/port/out/ComprobanteEvidenciaPort.java
@@ -3,12 +3,8 @@ package ec.dalara.factucore.application.port.out;
 import ec.dalara.factucore.application.port.out.sri.SriResponse;
 
 public interface ComprobanteEvidenciaPort {
-
-    String guardarXmlFirmado(Long comprobanteId, String xmlFirmado);
-
-    String guardarRespuestaSriRecepcion(Long comprobanteId, SriResponse respuesta);
-
-    String guardarRespuestaSriAutorizacion(Long comprobanteId, SriResponse respuesta);
-
-    String guardarRide(Long comprobanteId, byte[] pdf);
+    String guardarXmlGenerado(Long comprobanteId, String xmlGenerado, String usuario);
+    String guardarXmlFirmado(Long comprobanteId, String xmlFirmado, String usuario);
+    String guardarRespuestaSriAutorizacion(Long comprobanteId, SriResponse respuesta, String usuario);
+    String guardarRide(Long comprobanteId, byte[] pdf, String usuario);
 }
