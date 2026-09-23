d7c3b4cfdefe345a96b31539b6f8571e032dd5df
ldanny19 <103215792+ldanny19@users.noreply.github.com>
2026-09-23 16:10:17 -0500

    fix: completar mapeo de ElementoXsd

--- facturador/src/main/java/ec/dalara/factucore/application/mapper/ElementoXsdMapper.java
+++ facturador/src/main/java/ec/dalara/factucore/application/mapper/ElementoXsdMapper.java
@@ -1,10 +1,40 @@
 package ec.dalara.factucore.application.mapper;
+
 import org.mapstruct.Mapper;
+
 import ec.dalara.factucore.application.contract.request.ElementoXsdRequest;
 import ec.dalara.factucore.application.contract.response.ElementoXsdResponse;
 import ec.dalara.factucore.domain.documentoxsd.ElementoXsdModel;
-@Mapper(config=MapStructApplicationConfig.class)
+
+@Mapper(config = MapStructApplicationConfig.class)
 public interface ElementoXsdMapper {
- default ElementoXsdModel toModel(ElementoXsdRequest r){if(r==null)return null;return new ElementoXsdModel(null,r.getIdVersionDocumentoXsd(),r.getIdElementoPadre(),r.getNombre(),r.getTipoDato(),r.getOrden(),r.getObligatorio(),r.getRepetible(),r.getMinOcurrencias(),r.getMaxOcurrencias(),r.getLongitudMinima(),r.getLongitudMaxima(),r.getDigitosTotales(),r.getDecimales(),r.getValorMinimo(),r.getValorMaximo(),r.getPatron());}
- ElementoXsdResponse toResponse(ElementoXsdModel model);
+
+    default ElementoXsdModel toModel(ElementoXsdRequest r) {
+        if (r == null) {
+            return null;
+        }
+
+        return new ElementoXsdModel(
+                null,
+                r.getIdVersionDocumentoXsd(),
+                r.getIdElementoPadre(),
+                r.getNombre(),
+                r.getTipoDato(),
+                r.getOrden(),
+                r.getObligatorio(),
+                r.getRepetible(),
+                r.getMinOcurrencias(),
+                r.getMaxOcurrencias(),
+                r.getLongitudMinima(),
+                r.getLongitudMaxima(),
+                r.getDigitosTotales(),
+                r.getDecimales(),
+                r.getValorMinimo(),
+                r.getValorMaximo(),
+                r.getPatron(),
+                r.getFechaInicio(),
+                r.getFechaFin());
+    }
+
+    ElementoXsdResponse toResponse(ElementoXsdModel model);
 }
