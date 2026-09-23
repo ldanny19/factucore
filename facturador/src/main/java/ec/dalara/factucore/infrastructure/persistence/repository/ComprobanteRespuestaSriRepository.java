e6bdc666ab08e19c287a74d27f9aedea473c71b0
ldanny19 <103215792+ldanny19@users.noreply.github.com>
2026-09-22 23:28:22 -0500

    Elimina repository no contemplado en V1

--- facturador/src/main/java/ec/dalara/factucore/infrastructure/persistence/repository/ComprobanteRespuestaSriRepository.java
+++
@@ -1,6 +0,0 @@
-package ec.dalara.factucore.infrastructure.persistence.repository;
-
-import ec.dalara.factucore.infrastructure.persistence.entity.ComprobanteRespuestaSri;
-
-public interface ComprobanteRespuestaSriRepository extends BaseRepository<ComprobanteRespuestaSri, Long> {
-}
