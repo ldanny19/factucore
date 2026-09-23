1c9258b296302abf884e2648771a57d5572bafe7
ldanny19 <103215792+ldanny19@users.noreply.github.com>
2026-09-22 23:27:30 -0500

    Alinea puerto de evidencias al almacenamiento externo

--- facturador/src/main/java/ec/dalara/factucore/application/port/out/ComprobanteEvidenciaPort.java
+++ facturador/src/main/java/ec/dalara/factucore/application/port/out/ComprobanteEvidenciaPort.java
@@ -4,11 +4,11 @@ import ec.dalara.factucore.application.port.out.sri.SriResponse;
 
 public interface ComprobanteEvidenciaPort {
 
-    void guardarXmlGenerado(Long comprobanteId, String xml);
-
     void guardarXmlFirmado(Long comprobanteId, String xmlFirmado);
 
     void guardarRespuestaSriRecepcion(Long comprobanteId, SriResponse respuesta);
 
     void guardarRespuestaSriAutorizacion(Long comprobanteId, SriResponse respuesta);
+
+    void guardarRide(Long comprobanteId, byte[] pdf);
 }
