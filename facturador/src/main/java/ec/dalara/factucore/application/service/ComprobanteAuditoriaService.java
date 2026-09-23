afe2c8e43c41f230bc75d524bae8c0524b0c6d57
ldanny19 <103215792+ldanny19@users.noreply.github.com>
2026-09-23 15:20:54 -0500

    feat(facturador): restringir ComprobanteAuditoria a solo lectura

--- facturador/src/main/java/ec/dalara/factucore/application/service/ComprobanteAuditoriaService.java
+++ facturador/src/main/java/ec/dalara/factucore/application/service/ComprobanteAuditoriaService.java
@@ -1,6 +1,8 @@
 package ec.dalara.factucore.application.service;
 
 import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
+import ec.dalara.factucore.application.ApplicationException;
 
 import org.springframework.stereotype.Service;
 
@@ -19,6 +21,30 @@ public class ComprobanteAuditoriaService extends BaseService<ComprobanteAuditori
 	@Override
 	protected BaseRepository<ComprobanteAuditoria, Long> getRepository() {
 		return comprobanteAuditoriaRepository;
+	}
+
+	@Override
+	@Transactional
+	public ComprobanteAuditoria guardar(ComprobanteAuditoria entidad) {
+		throw new ApplicationException("FACTUCORE.COMPROBANTE_AUDITORIA.SOLO_LECTURA");
+	}
+
+	@Override
+	@Transactional
+	public ComprobanteAuditoria inactivar(Long id) {
+		throw new ApplicationException("FACTUCORE.COMPROBANTE_AUDITORIA.SOLO_LECTURA");
+	}
+
+	@Override
+	@Transactional
+	public ComprobanteAuditoria reactivar(Long id) {
+		throw new ApplicationException("FACTUCORE.COMPROBANTE_AUDITORIA.SOLO_LECTURA");
+	}
+
+	@Override
+	@Transactional
+	public ComprobanteAuditoria eliminar(Long id) {
+		throw new ApplicationException("FACTUCORE.COMPROBANTE_AUDITORIA.SOLO_LECTURA");
 	}
 
 	public List<ComprobanteAuditoria> listarPorComprobante(Long comprobanteId) {
