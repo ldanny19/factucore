8891eeaf26cc41dab2df47ae8ee622b642d6f9e4
ldanny19 <103215792+ldanny19@users.noreply.github.com>
2026-09-23 16:10:48 -0500

    fix: completar mapeo de EnumeracionXsd

--- facturador/src/main/java/ec/dalara/factucore/application/mapper/EnumeracionXsdMapper.java
+++ facturador/src/main/java/ec/dalara/factucore/application/mapper/EnumeracionXsdMapper.java
@@ -1,10 +1,15 @@
 package ec.dalara.factucore.application.mapper;
+
 import org.mapstruct.Mapper;
 import ec.dalara.factucore.application.contract.request.EnumeracionXsdRequest;
 import ec.dalara.factucore.application.contract.response.EnumeracionXsdResponse;
 import ec.dalara.factucore.domain.documentoxsd.EnumeracionXsdModel;
-@Mapper(config=MapStructApplicationConfig.class)
+
+@Mapper(config = MapStructApplicationConfig.class)
 public interface EnumeracionXsdMapper {
- default EnumeracionXsdModel toModel(EnumeracionXsdRequest r){if(r==null)return null;return new EnumeracionXsdModel(r.getIdElementoXsd(),r.getValor(),r.getDescripcion(),r.getOrden());}
- EnumeracionXsdResponse toResponse(EnumeracionXsdModel model);
+    default EnumeracionXsdModel toModel(EnumeracionXsdRequest r) {
+        if (r == null) return null;
+        return new EnumeracionXsdModel(null, r.getIdElementoXsd(), r.getValor(), r.getDescripcion(), r.getOrden());
+    }
+    EnumeracionXsdResponse toResponse(EnumeracionXsdModel model);
 }
