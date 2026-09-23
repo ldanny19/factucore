fa6ac0b191ba8dba8cb30f0c14922181dc13d483
ldanny19 <103215792+ldanny19@users.noreply.github.com>
2026-09-22 22:35:45 -0500

    fix: limpiar imports del scheduler

--- facturador/src/main/java/ec/dalara/factucore/application/service/WorkflowReprocessScheduler.java
+++ facturador/src/main/java/ec/dalara/factucore/application/service/WorkflowReprocessScheduler.java
@@ -6,9 +6,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import ec.dalara.factucore.application.port.out.WorkflowExecutionPort;
-import ec.dalara.factucore.domain.workflow.EstadoProceso;
 import ec.dalara.factucore.infrastructure.configuration.workflow.WorkflowReprocessProperties;
-import ec.dalara.factucore.infrastructure.persistence.entity.Comprobante;
 import ec.dalara.factucore.infrastructure.persistence.entity.Comprobante;
 import ec.dalara.factucore.infrastructure.persistence.repository.ComprobanteRepository;
 import lombok.RequiredArgsConstructor;
