f930575b68e504567cfd2d78b01d54cabdf17eb8
ldanny19 <103215792+ldanny19@users.noreply.github.com>
2026-09-23 16:06:55 -0500

    fix: completar mapeo de VersionDocumentoXsd

--- facturador/src/main/java/ec/dalara/factucore/infrastructure/mapper/entity/VersionDocumentoXsdEntityMapper.java
+++ facturador/src/main/java/ec/dalara/factucore/infrastructure/mapper/entity/VersionDocumentoXsdEntityMapper.java
@@ -1,12 +1,33 @@
 package ec.dalara.factucore.infrastructure.mapper.entity;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
 import ec.dalara.factucore.domain.documentoxsd.VersionDocumentoXsdModel;
 import ec.dalara.factucore.infrastructure.persistence.entity.VersionDocumentoXsd;
-@Mapper(config=ec.dalara.factucore.infrastructure.mapper.MapStructInfrastructureConfig.class)
+
+@Mapper(config = ec.dalara.factucore.infrastructure.mapper.MapStructInfrastructureConfig.class)
 public interface VersionDocumentoXsdEntityMapper {
- default VersionDocumentoXsdModel toModel(VersionDocumentoXsd e){if(e==null)return null;return new VersionDocumentoXsdModel(e.getId(),e.getDocumentoXsd().getId(),e.getVersion(),e.getNamespaceXml(),e.getElementoRaiz(),e.getFechaInicio(),e.getFechaFin());}
- @Mapping(target="id",ignore=true)
- @Mapping(target="documentoXsd", ignore=true)
- VersionDocumentoXsd toEntity(VersionDocumentoXsdModel model);
+
+    default VersionDocumentoXsdModel toModel(VersionDocumentoXsd e) {
+        if (e == null) {
+            return null;
+        }
+
+        return new VersionDocumentoXsdModel(
+                e.getId(),
+                e.getDocumentoXsd().getId(),
+                e.getVersion(),
+                e.getNombreArchivo(),
+                e.getNamespaceXml(),
+                e.getElementoRaiz(),
+                e.getPlantillaJson(),
+                e.getEsquemaJson(),
+                e.getFechaInicio(),
+                e.getFechaFin());
+    }
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "documentoXsd", ignore = true)
+    VersionDocumentoXsd toEntity(VersionDocumentoXsdModel model);
 }
