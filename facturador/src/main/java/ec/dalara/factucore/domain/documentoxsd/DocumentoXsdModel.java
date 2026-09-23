63e116ffcbb60c37db725ba39917e224d465857c
ldanny19 <103215792+ldanny19@users.noreply.github.com>
2026-09-23 18:29:52 -0500

    feat(documento-xsd): agregar prefijo de archivo al tipo de documento

--- facturador/src/main/java/ec/dalara/factucore/domain/documentoxsd/DocumentoXsdModel.java
+++ facturador/src/main/java/ec/dalara/factucore/domain/documentoxsd/DocumentoXsdModel.java
@@ -4,31 +4,38 @@ import java.util.Objects;
 
 public class DocumentoXsdModel {
 
-	private final String codigo;
-	private final String nombre;
-	private final String descripcion;
-	private final String tipoDocumento;
+    private final String codigo;
+    private final String nombre;
+    private final String descripcion;
+    private final String tipoDocumento;
+    private final String prefijoArchivo;
 
-	public DocumentoXsdModel(String codigo, String nombre, String descripcion, String tipoDocumento) {
-		this.codigo = Objects.requireNonNull(codigo);
-		this.nombre = Objects.requireNonNull(nombre);
-		this.tipoDocumento = Objects.requireNonNull(tipoDocumento);
-		this.descripcion = descripcion;
-	}
+    public DocumentoXsdModel(String codigo, String nombre, String descripcion, String tipoDocumento,
+            String prefijoArchivo) {
+        this.codigo = Objects.requireNonNull(codigo);
+        this.nombre = Objects.requireNonNull(nombre);
+        this.tipoDocumento = Objects.requireNonNull(tipoDocumento);
+        this.descripcion = descripcion;
+        this.prefijoArchivo = Objects.requireNonNull(prefijoArchivo);
+    }
 
-	public String getCodigo() {
-		return codigo;
-	}
+    public String getCodigo() {
+        return codigo;
+    }
 
-	public String getNombre() {
-		return nombre;
-	}
+    public String getNombre() {
+        return nombre;
+    }
 
-	public String getDescripcion() {
-		return descripcion;
-	}
+    public String getDescripcion() {
+        return descripcion;
+    }
 
-	public String getTipoDocumento() {
-		return tipoDocumento;
-	}
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public String getPrefijoArchivo() {
+        return prefijoArchivo;
+    }
 }
