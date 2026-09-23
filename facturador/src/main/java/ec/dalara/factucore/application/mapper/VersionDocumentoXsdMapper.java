90a0b5ecb5f25a08e49def51e3fedbcab6491763
ldanny19 <103215792+ldanny19@users.noreply.github.com>
2026-09-23 16:01:14 -0500

    ajusta mapper de version de documento XSD

--- facturador/src/main/java/ec/dalara/factucore/application/mapper/VersionDocumentoXsdMapper.java
+++ facturador/src/main/java/ec/dalara/factucore/application/mapper/VersionDocumentoXsdMapper.java
@@ -1,11 +1,31 @@
 package ec.dalara.factucore.application.mapper;
+
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+
 import ec.dalara.factucore.application.contract.request.VersionDocumentoXsdRequest;
 import ec.dalara.factucore.application.contract.response.VersionDocumentoXsdResponse;
 import ec.dalara.factucore.domain.documentoxsd.VersionDocumentoXsdModel;
-@Mapper(config=MapStructApplicationConfig.class)
+
+@Mapper(config = MapStructApplicationConfig.class)
 public interface VersionDocumentoXsdMapper {
-    default VersionDocumentoXsdModel toModel(VersionDocumentoXsdRequest r) { if(r==null)return null; return new VersionDocumentoXsdModel(null,r.getIdDocumentoXsd(),r.getVersion(),r.getNamespaceXml(),r.getElementoRaiz(),r.getFechaInicio(),r.getFechaFin()); }
+
+    default VersionDocumentoXsdModel toModel(VersionDocumentoXsdRequest request) {
+        if (request == null) {
+            return null;
+        }
+
+        return new VersionDocumentoXsdModel(
+                null,
+                request.getIdDocumentoXsd(),
+                request.getVersion(),
+                request.getNombreArchivo(),
+                request.getNamespaceXml(),
+                request.getElementoRaiz(),
+                request.getPlantillaJson(),
+                request.getEsquemaJson(),
+                request.getFechaInicio(),
+                request.getFechaFin());
+    }
+
     VersionDocumentoXsdResponse toResponse(VersionDocumentoXsdModel model);
 }
