4c5d95a1db5dcc793a2a3d75a3beb132ee154faf
ldanny19 <103215792+ldanny19@users.noreply.github.com>
2026-09-23 16:10:50 -0500

    fix: completar mapeo entidad de EnumeracionXsd

--- facturador/src/main/java/ec/dalara/factucore/infrastructure/mapper/entity/EnumeracionXsdEntityMapper.java
+++ facturador/src/main/java/ec/dalara/factucore/infrastructure/mapper/entity/EnumeracionXsdEntityMapper.java
@@ -1,12 +1,17 @@
 package ec.dalara.factucore.infrastructure.mapper.entity;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import ec.dalara.factucore.domain.documentoxsd.EnumeracionXsdModel;
 import ec.dalara.factucore.infrastructure.persistence.entity.EnumeracionXsd;
-@Mapper(config=ec.dalara.factucore.infrastructure.mapper.MapStructInfrastructureConfig.class)
+
+@Mapper(config = ec.dalara.factucore.infrastructure.mapper.MapStructInfrastructureConfig.class)
 public interface EnumeracionXsdEntityMapper {
- default EnumeracionXsdModel toModel(EnumeracionXsd e){if(e==null)return null;return new EnumeracionXsdModel(e.getElementoXsd().getId(),e.getValor(),e.getDescripcion(),e.getOrden());}
- @Mapping(target="id",ignore=true)
- @Mapping(target="elementoXsd", ignore=true)
- EnumeracionXsd toEntity(EnumeracionXsdModel model);
+    default EnumeracionXsdModel toModel(EnumeracionXsd e) {
+        if (e == null) return null;
+        return new EnumeracionXsdModel(e.getId(), e.getElementoXsd().getId(), e.getValor(), e.getDescripcion(), e.getOrden());
+    }
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "elementoXsd", ignore = true)
+    EnumeracionXsd toEntity(EnumeracionXsdModel model);
 }
