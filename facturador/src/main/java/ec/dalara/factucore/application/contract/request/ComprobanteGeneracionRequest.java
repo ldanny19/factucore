91e3828f5f08e67eb437f62d2eff5c5adea4cb5a
ldanny19 <103215792+ldanny19@users.noreply.github.com>
2026-09-23 17:30:13 -0500

    Implementar idempotencia por idTransaccion

--- facturador/src/main/java/ec/dalara/factucore/application/contract/request/ComprobanteGeneracionRequest.java
+++ facturador/src/main/java/ec/dalara/factucore/application/contract/request/ComprobanteGeneracionRequest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,6 +19,7 @@ import lombok.NoArgsConstructor;
 public class ComprobanteGeneracionRequest {
 
 	@NotBlank
+	@Size(max = 100)
 	private String idTransaccion;
 
 	@NotNull
